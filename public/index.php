6a4e7c99487a9a658d8e17f8e105bda111f12828
EC2 Default User <ec2-user@ip-172-31-1-118.us-west-2.compute.internal>
2020-08-24 14:15:02 +0000

    解决CORS问题

--- public/index.php
+++ public/index.php
@@ -10,6 +10,12 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
+//Resolve CORS issue
+header('Access-Control-Allow-Origin:*');
+// 响应类型  
+header('Access-Control-Allow-Methods:*');
+// 响应头设置  
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
